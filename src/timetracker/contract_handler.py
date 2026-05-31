4f163823021c561cdca7ea6eaae8aa2d705c07de
MaiFinn <maier.finn@t-online.de>
2026-05-31 18:15:07 +0200

    Update contract handler to use json func.

--- src/timetracker/contract_handler.py
+++ src/timetracker/contract_handler.py
@@ -1,47 +1,38 @@
-import json
 from dataclasses import asdict, dataclass
 from pathlib import Path
 
+from timetracker.storage.json_storage import save_json
+
+
 @dataclass
 class Contract:
-    "Container class for contract infos."
+    """Container class for contract infos."""
 
     company_name: str
     start_date: str
     salary_per_hour: float
     hours_per_week: float
 
+
 def create_contract(
-        company_name: str,
-        start_date: str,
-        salary_per_hour: float,
-        hours_per_week: float,
-        file_path: Path | None = None,
+    company_name: str,
+    start_date: str,
+    salary_per_hour: float,
+    hours_per_week: float,
+    file_path: Path | None = None,
 ) -> None:
-    """
-    Create a new contract file.
+    """Create a new contract file."""
 
-    Args:
-        company_name (str): Name of the company
-        start_date (str): Contract starting date
-        salar_per_hour (float): Hourly compensation
-        hours_per_week (float): Working hours per week
-    """
     if file_path is None:
+        project_root = Path(__file__).resolve().parents[2]
+        artifacts_dir = project_root / "artifacts"
+        file_path = artifacts_dir / "contract.json"
 
-        PROJECT_ROOT = Path(__file__).resolve().parents[2]
-        ARTIFACTS_DIR = PROJECT_ROOT / "artifacts"
-        ARTIFACTS_DIR.mkdir(exist_ok=True)
-        file_path = ARTIFACTS_DIR / "contract.json"
-    
     contract_details = Contract(
-    company_name=company_name,
-    start_date=start_date,
-    salary_per_hour=salary_per_hour,
-    hours_per_week=hours_per_week
+        company_name=company_name,
+        start_date=start_date,
+        salary_per_hour=salary_per_hour,
+        hours_per_week=hours_per_week,
     )
 
-    contract_dict = asdict(contract_details)
-
-    with open(file_path, "w") as f:
-        json.dump(contract_dict, f)
+    save_json(file_path, asdict(contract_details))
